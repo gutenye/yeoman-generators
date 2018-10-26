28cba0392931ab8ebf33f35579a5c4c0c5dd7db0
Guten Ye <ywzhaifei@gmail.com>
2018-10-26 16:30:28 +0800

    use config lib

--- app/server/templates/src/vendor/index.ts
+++ app/server/templates/src/vendor/index.ts
@@ -1,3 +1,4 @@
-export { default as appRc } from '../app.config'
+import * as config from 'config'
 export { default as pubsub } from './pubsub'
 export { default as log } from './log'
+export { config }
